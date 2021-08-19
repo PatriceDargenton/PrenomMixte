d6a8f9114de32771b6aab0ed6303640b4e342daa
Patrice DARGENTON <patrice.dargenton@free.fr>
2021-08-19 11:25:45 +0200

    Ajout projet Tests unitaires

---
+++ UnitTestPrenomMixte/UnitTestPrenomMixte.vb
@@ -0,0 +1,14 @@
+﻿
+Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTestPrenomMixte
+
+    <TestMethod()>
+    Public Sub TestMethod1()
+
+        Assert.AreEqual(True, True)
+
+    End Sub
+
+End Class
